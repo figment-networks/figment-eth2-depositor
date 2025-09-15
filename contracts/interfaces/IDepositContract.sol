acf35d9f03a8354df7b389892faeaddc3f6dd0a5
Matt Kornatz <mkornatz@figment.io>
2025-09-15 11:06:29 -0400

    Modifies URL in comments for deposit contract interface

--- contracts/interfaces/IDepositContract.sol
+++ contracts/interfaces/IDepositContract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 
 // This interface is designed to be compatible with the Vyper version.
 /// @notice This is the Ethereum 2.0 deposit contract interface.
-/// For more information see the Phase 0 specification under https://github.com/ethereum/eth2.0-specs
+/// For more information see https://github.com/ethereum/consensus-specs/tree/master/solidity_deposit_contract
 interface IDepositContract {
     /// @notice A processed deposit event.
     event DepositEvent(
